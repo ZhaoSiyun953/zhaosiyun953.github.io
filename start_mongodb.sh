271ac752c6369dff20b7c196159be4a11eec6ab9
yujie0121 <1150508355@qq.com>
2024-11-06 22:28:29 +0800

    remove some files & rebuild the project

--- start_mongodb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log
--- start_node.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /Users/zhaosiyun/Desktop/IMM\(2\)/WD/siyunPorfolio
-npm start
